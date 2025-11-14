4b46686c35d4e8373dcaac7de515ee39b691375c
brunna.dornelles <brunnadornelles336@gmail.com>
2025-11-14 10:26:49 -0300

    Refatoração em versões e plugins no pom, para uso de anotações Lombok.

--- crud-pessoa/src/main/java/com/br/crud_pessoa/model/Pessoa.java
+++ crud-pessoa/src/main/java/com/br/crud_pessoa/model/Pessoa.java
@@ -1,11 +1,36 @@
 package com.br.crud_pessoa.model;
 
-import jakarta.persistence.Entity;
-import lombok.Data;
 
-@Data
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
 @Entity
+@Table(name = "pessoas", uniqueConstraints = {@UniqueConstraint(columnNames = "cpf")})
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O campo nome deve ser obrigatório")
+    private String nome;
+
+    private LocalDate dataNascimento;
+
+
+
+
 
 
 }
