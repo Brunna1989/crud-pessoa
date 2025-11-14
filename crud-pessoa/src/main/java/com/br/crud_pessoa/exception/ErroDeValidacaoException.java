03039fe18007343e56ef11691fb1ca5b9435a2ea
brunna.dornelles <brunnadornelles336@gmail.com>
2025-11-14 17:55:04 -0300

    Exclusão de classe desnecessária e adição de readme.md com instruções.

--- crud-pessoa/src/main/java/com/br/crud_pessoa/exception/ErroDeValidacaoException.java
+++
@@ -1,8 +0,0 @@
-package com.br.crud_pessoa.exception;
-
-public class ErroDeValidacaoException extends RuntimeException{
-    public ErroDeValidacaoException(String mensagem) {
-        super(mensagem);
-
-    }
-}
