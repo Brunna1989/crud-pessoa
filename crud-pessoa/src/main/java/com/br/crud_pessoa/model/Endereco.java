1ccf2f8fc8e3e4bbaeea5094f9fe0218289e2e8d
brunna.dornelles <brunnadornelles336@gmail.com>
2025-11-14 11:22:36 -0300

    Adicionando classe model: Endereço.

--- crud-pessoa/src/main/java/com/br/crud_pessoa/model/Endereco.java
+++ crud-pessoa/src/main/java/com/br/crud_pessoa/model/Endereco.java
@@ -1,11 +1,39 @@
 package com.br.crud_pessoa.model;
 
-import jakarta.persistence.Entity;
-import lombok.Data;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 
-@Data
 @Entity
+@Table(name = "enderecos")
 public class Endereco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O nome da rua deve ser obrigatório.")
+    private String rua;
+
+    @NotBlank(message = "O campo número deve ser obrigatório.")
+    private String numero;
+
+    @NotBlank(message = "O campo bairro deve ser obrigatório.")
+    private String bairro;
+
+    @NotBlank(message = "O campo cidade deve ser obrigatório.")
+    private String cidade;
+
+    @NotBlank(message = "O campo CEP deve ser obrigatório.")
+    private String cep;
+
+    @ManyToMany
+    @JoinColumn(name = "pessoa_id")
+    private Pessoa pessoa;
+
+
+
+
+
 
 
 }
